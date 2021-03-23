2f7d770a09fb299dee06f3cc0be5dca74878de53
inenashev <nenashevivan9088@gmail.com>
2021-03-23 18:29:02 +0600

    Тест

--- UI_one.js
+++ UI_one.js
@@ -1,15 +1,11 @@
-const { chromium, firefox} = require('playwright');
+const { webkit } = require('playwright');
 
-(async() => {
-    for (const browserType of [firefox, chromium]) {
-const browser = await browserType.launch({
-headless: false });
-const page = await browser.newPage();
-await page.goto('https://alfa.gemsdev.ru');
-await page.screenshot({
-    path: 'screenshot$.png',
-});
-await browser.close();
-console.log ('success' + browserType.name());
-    }
+(async () => {
+  const browser = await webkit.launch({
+    headless: false });
+  const page = await browser.newPage();
+  await page.goto('http://alfa.gemsdev.ru/');
+  await page.waitForNavigation();
+  await page.screenshot({ path: `example.png` });
+  await browser.close();
 })();
